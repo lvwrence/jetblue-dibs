3399144b8e89ed0a38fbe69ca0fb535fe31fdb3d
Lawrence Wu <lawrence@lawrencewu.me>
2015-11-07 03:07:00 -0500

    get bsic react loading
    Why or how?
    *

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,26 +1,12 @@
 var gulp = require('gulp');
-var babelify = require('babelify');
 var browserify = require('browserify');
+var babelify = require('babelify');
 var source = require('vinyl-source-stream');
-var rename = require('gulp-rename');
 
-gulp.task('browserify', function() {
-  var b = browserify({
-    entries: './src/app.js',
-    paths: ['./node_modules', './src/'],
-    debug: true,
-    transform: [babelify]
-  });
-
-  return b.bundle()
-    .on('error', function(err) {
-      console.log(err.toString());
-      this.emit('end');
-    })
-    .pipe(source('./src/js/app.js'))
-        .pipe(rename('app.js'))
-
-    .pipe(gulp.dest('./static/js/'));
+gulp.task('default', function () {
+    return browserify({entries: './src/app.jsx', extensions: ['.jsx'], debug: true})
+        .transform('babelify', {presets: ['es2015', 'react']})
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('static/js'));
 });
-
-gulp.task('default', ['browserify']);

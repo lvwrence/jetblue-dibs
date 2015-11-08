e9c025c36818d6916b62e7d27df758414a178901
Jesse Riemann <jesse.riemann@gmail.com>
2015-11-08 04:00:58 -0500

    code_to_hotel

--- code_to_hotel.py
+++ code_to_hotel.py
@@ -1,5 +1,39 @@
+# -*- coding: utf-8 -*-
 
+CODE_TO_CITY_MAPPING = {
+        'AUA':'http://www.iammadein.com/aruba/wp-content/uploads/2015/03/amsterdam-manor-resort.jpg',
+        'AZS':'http://r-ec.bstatic.com/images/hotel/840x460/414/41476196.jpg',
+        'BDA':'http://media-cdn.tripadvisor.com/media/photo-s/04/24/78/f1/cambridge-beaches.jpg',
+        'BGI':'http://www.myvacationpages.com/data/listing-images/7890',
+        'CHS':'http://www.charlestonairport.place.hyatt.com/content/dam/PropertyWebsites/place/chszc/Media/All/Hyatt-Place-Charleston-Airport-Convention-Center-North-P024-Doubles-Overall.masthead-feature-panel-medium.jpg',
+        'CUN':'http://www.tnetnoc.com/hotelphotos/651/1275651/2241284-Adhara-Hacienda-Cancun-Hotel-Exterior-1-DEF.jpg',
+        'CUR':'http://curacaovakantieinfo.nl/wp-content/uploads/2014/10/Avila-beach.jpg',
+        'FLL':'https://assets.hipmunk.com/assets/hotel_image/v1/55489fd1cfcf1e3da8064e09.jpg',
+        'GCM':'http://www.thescubanews.com/wp-content/uploads/2013/10/greenglobe3.jpg',
+        'GND':'https://jamaicagetawaytravels.com/wp-content/uploads/2015/01/jgat-coyaba-beach-resort2.jpg',
+        'JFK':'http://r-ec.bstatic.com/images/hotel/840x460/867/8674778.jpg',
+        'LAS':'https://shopofmagic.com/media/com_ohanah/attachments/412698961-Alexis02.jpg',
+        'LAX':'http://media-cdn.tripadvisor.com/media/photo-s/07/77/d4/3b/traditional-double-queen.jpg',
+        'LIR':'http://media-cdn.tripadvisor.com/media/photo-s/02/c1/a0/fc/best-western-el-sitio.jpg',
+        'LRM':'http://www.islandhideaways.com/wire/assets/files/4186/la-naturaleza-at-casa-de-2.jpg',
+        'MBJ':'http://www.cheapcaribbean.com/upload/lo_309/re_3326/file_4883047670030595321620x310.jpg',
+        'MCO':'http://exp.cdn-hotels.com/hotels/3000000/2930000/2929000/2928902/2928902_100_z.jpg',
+        'MSY':'http://cache.marriott.com/propertyimages/m/msyac/phototour/msyac_phototour34.jpg?Log=1',
+        'NAS':'http://www.atlantisbahamas.com/media-versions/39e/720/39e72071372f0957b8e8af9f27e6d0fb/CoralTowers_GuestRoom.jpg',
+        'PBI':'http://hotelsinwichitafallstx.h.bestwestern.com/content/dam/bwi-properties/dom/usa/tx/44657/44657-lobby-01-g-best-western-plus-university-inn-and-suites.jpg',
+        'PLS':'http://www.islandhideaways.com/wire/assets/files/7437/beach-house-at-parrot-cay-4.jpg',
+        'POP':'http://www.2look4beds.com/img/hotel/23199964/1/Agualina-Kite-Resort-Studio-1.jpg',
+        'PUJ':'https://weddingphotographypuntacana.files.wordpress.com/2014/01/al-sol-del-mar-cap-cana-wedding-photography-punta-cana-ambrogetti-ameztoy-48-of-52.jpg',
+        'RSW':'http://sanibelanchorinn.com/wp-content/uploads/2013/12/Anchor-Inn-Cottage-22-110.jpg',
+        'SAN':'http://sandiego.andaz.hyatt.com/content/dam/PropertyWebsites/andaz/sanas/Media/All/SANAS_P055_Suite-1280x427.jpg',
+        'SDQ':'http://media-cdn.tripadvisor.com/media/photo-o/04/5f/6e/7d/tonemapped.jpg',
+        'SFO':'http://r-ec.bstatic.com/images/hotel/840x460/382/38206761.jpg',
+        'SJU':'http://www.condadopalm.com/d/condadopalm/media/rooms/130410_BW_SanJuanPR_010.jpg',
+        'SXM':'http://media-cdn.tripadvisor.com/media/photo-s/02/a6/38/5e/filename-img-0901-jpg.jpg',
+        'TPA':'http://assets.alofttampadowntown.com/XLGallery/md/alf3896gr-156556-Aloft-Tampa-Double-Guest-Room.jpg',
+        'UVF':'',
+        'LGB':'',
+        'BOS':''
+        }
 
-CODE_TO_HOTEL = {
-        'AUA':'http://www.iammadein.com/aruba/wp-content/uploads/2015/03/amsterdam-manor-resort.jpg'
-}
+ALL_CODES = CODE_TO_CITY_MAPPING.keys()
